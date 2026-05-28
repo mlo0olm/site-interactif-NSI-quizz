27a787239334e75de477936ba3bd69c83f8f5ef0
Earatozytet <gaspard.lemonde@gmail.com>
2026-05-28 11:11:45 +0200

    Premier script JS

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    function clickAnswer() {
+
+    }
+
+	document.getElementById("button_reponse").addEventListener("click", clickAnswer);
+})
