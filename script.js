ab12162070f401d631b5e2b009d03ef87ee293d7
Earatozytet <gaspard.lemonde@gmail.com>
2026-05-28 11:40:47 +0200

    Génération des questions -- Work in Progress

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const questions = [
+const list_questions = [
     ["Quel est la bonne réponse ?", ["A", "B", "C"], "B"] // Questions, [liste de réponses possibles], bonne réponse
-]
-let index_question = 0
+];
+let index_question = -1;
 
 document.addEventListener('DOMContentLoaded', () => {
+    function new_question() {
+        index_question ++;
+        document.getElementById("question").innerText = list_questions[index_question][0];
+        const div_possible = document.getElementById("possibilites")
+        var div = document.createElement("div");
+        var input = document.createElement("input")
+        input.id = "ownerName" + buttons;
+        input.className = "form-control";
+        document.body.appendChild(input);
+
+
+    }
 
     function clickAnswer() {
         let inputs = document.querySelectorAll("fieldset input[name='réponse']");
@@ -14,6 +26,6 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
-
+    new_question()
 	document.getElementById("button_reponse").addEventListener("click", clickAnswer);
 })
