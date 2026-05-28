78056c570cafd58183e806a0f52130d8273e30f1
Earatozytet <gaspard.lemonde@gmail.com>
2026-05-28 11:33:12 +0200

    Ajout d'une vérification dans la console de l'élément coché.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    function clickAnswer() {
 
+    function clickAnswer() {
+        let inputs = document.querySelectorAll("fieldset input[name='réponse']");
+        inputs.forEach((element) => {
+            console.log(element.checked);
+            if (element.checked == true) {
+                console.log(element);
+            }
+        });
     }
 
 	document.getElementById("button_reponse").addEventListener("click", clickAnswer);
