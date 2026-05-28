f36cb568d54daca16bf500cc972a0cdb2f5104f5
Earatozytet <gaspard.lemonde@gmail.com>
2026-05-28 21:26:13 +0200

    Génération des questions (ajout de la modification dans le html des réponses possibles et d'un texte affichée si la réponse est correcte ou incorrecte) -- Work in Progress

--- script.js
+++ script.js
@@ -7,25 +7,55 @@ document.addEventListener('DOMContentLoaded', () => {
     function new_question() {
         index_question ++;
         document.getElementById("question").innerText = list_questions[index_question][0];
-        const div_possible = document.getElementById("possibilites")
-        var div = document.createElement("div");
-        var input = document.createElement("input")
-        input.id = "ownerName" + buttons;
-        input.className = "form-control";
-        document.body.appendChild(input);
+        const div_possible = document.getElementById("possibilites");
+        div_possible.innerHTML = ""; // TODO : manière plus propre de vider la div ???
 
+		index = 0;
+		list_questions[index_question][1].forEach(
+			(element) => {
+				var div = document.createElement("div");
 
+				var input = document.createElement("input");
+				input.type = "radio";
+				input.name = "réponse";
+				input.id = "reponse"+index;
+				input.value = element;
+				/*
+				input.id = "ownerName" + buttons;
+				input.className = "form-control";
+				*/
+
+				var label = document.createElement("label");
+				label.htmlFor = "reponse"+index;
+				label.innerText = element;
+
+				div.appendChild(input);
+				div.appendChild(label);
+				div_possible.appendChild(div);
+
+				index ++;
+			}
+		)
     }
 
     function clickAnswer() {
         let inputs = document.querySelectorAll("fieldset input[name='réponse']");
         inputs.forEach((element) => {
-            console.log(element.checked);
             if (element.checked == true) {
-                console.log(element);
+                if (element.value == list_questions[index_question][2]){
+					console.log("Réponse correct");
+					document.getElementById("correct").hidden = false;
+				} else {
+					console.log("Réponse incorrect");
+					document.getElementById("incorrect").hidden = false;
+				}
             }
         });
     }
+
+	/*document.getElementById("correct").hidden = true;
+	document.getElementById("incorrect").hidden = true;*/
+
     new_question()
 	document.getElementById("button_reponse").addEventListener("click", clickAnswer);
 })
